7a8be10f20788c786c0c7ad9b2407e5dc6c23406
Sung-Numa03 <sungnumafroko@gmail.com>
2024-08-19 23:50:56 +0100

    Adding a function to get a friend

--- index.js
+++ index.js
@@ -2,14 +2,50 @@ const http = require('http');
 
 const PORT = 7000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'content-type': 'application/json',
-    });
-    res.end(JSON.stringify({
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: 'Gallileo Gallilea',
+    },
+    {
         id: 1,
         name: 'Sir Isaac Newton',
-    }));
+    },
+    {
+        id: 2,
+        name: 'Albert Eisntein',
+    },
+    {
+        id: 3,
+        name: 'Marie Curie',
+    }
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+    if(items[1] === 'friends') {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        if(items.length === 3) {
+            const friendsIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendsIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/html');
+        res.write('<ul>');
+        res.write('<li>Hello Isaac Newton</li>')
+        res.write('<li>What are your thoughts on astronomy!</li>')
+        res.write('</ul>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
